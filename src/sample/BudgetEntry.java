e2bf31a6cb468d95efcd60a7d982f91696739927
robertla234 <robertla234@yahoo.com>
2021-02-19 18:44:39 -0800

    mock up Table View for Budget Entries

---
+++ src/sample/BudgetEntry.java
@@ -0,0 +1,33 @@
+package sample;
+
+import java.util.Date;
+
+public class BudgetEntry {
+
+    private Date date;
+    private int amount;
+
+    public BudgetEntry(){
+    }
+
+    public BudgetEntry(Date date, int amount){
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+    }
+
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,23 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
+import javafx.scene.shape.Box;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+
+import java.util.Date;
 
 public class Main extends Application {
 
@@ -20,6 +25,42 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Hello World");
         BorderPane rootBPane = new BorderPane();
+
+
+        //Main Scene Area setup
+        //^--- Home ---
+            //Welcome Display for Home
+        StackPane homePane = new StackPane();
+        homePane.setStyle("-fx-background-color: #ff0000");
+        Label welHomeLabel = new Label("Welcome To Home");
+        welHomeLabel.setPadding(new Insets(10, 10, 10, 10));
+        welHomeLabel.setStyle("-fx-background-color: #B3B3B3");
+        homePane.getChildren().add(welHomeLabel);
+            //add homePane to Center side
+        rootBPane.setCenter(homePane);
+
+        //^--- Entries ---
+        BorderPane entryPane = new BorderPane();
+        entryPane.setStyle("-fx-background-color: #00ff00");
+            //Display Box for Current-In-Budget
+        Box CalcSum = new Box();
+        CalcSum.setHeight(20);
+        CalcSum.setWidth(100);
+        CalcSum.setStyle("-fx-padding: 10 10 10 10;");
+        entryPane.setTop(CalcSum);
+        entryPane.setMargin(CalcSum, new Insets(10));
+        entryPane.setAlignment(CalcSum, Pos.CENTER);
+            //Entry Table for Calc
+        TableView budgetEntries = new TableView();
+        TableColumn<BudgetEntry, Date> dateColumn = new TableColumn<>("Date");
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        TableColumn<BudgetEntry, Integer> amountColumn = new TableColumn<>("Amount");
+        amountColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        budgetEntries.getColumns().add(dateColumn);
+        budgetEntries.getColumns().add(amountColumn);
+        budgetEntries.getItems().add(new BudgetEntry(new Date(), 300));
+        budgetEntries.getItems().add(new BudgetEntry(new Date(), -200));
+        entryPane.setCenter(budgetEntries);
 
         //Left Navbar Setup
         VBox navBar = new VBox();
@@ -29,21 +70,21 @@ public class Main extends Application {
         Button btnHome = makeButton("Home");
         Button btnCalc = makeButton("Calculator");
         navBar.getChildren().addAll(btnHome, btnCalc);
-            //add navBar to Left side
+        //add navBar to Left side
         rootBPane.setLeft(navBar);
+        btnHome.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                rootBPane.setCenter(homePane);
+            }
+        });
+        btnCalc.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                rootBPane.setCenter(entryPane);
+            }
+        });
 
-        //Main Scene Area setup
-        //--- Home ---
-        StackPane homePane = new StackPane();
-        //VBox homePane = new VBox();
-        homePane.setStyle("-fx-background-color: #ff0000");
-        //Label welHomeLabel = new Label("Welcome to Home");
-        Label welHomeLabel = new Label("Welcome To Home");
-        welHomeLabel.setPadding(new Insets(10, 10, 10, 10));
-        welHomeLabel.setStyle("-fx-background-color: #B3B3B3");
-        homePane.getChildren().add(welHomeLabel);
-            //add homePane to Center side
-        rootBPane.setCenter(homePane);
 
         Scene scene = new Scene(rootBPane, 400, 300);
         primaryStage.setScene(scene);
@@ -67,3 +108,5 @@ public class Main extends Application {
         return btnMake;
     }
 }
+
+
