27a2062ed456b6fa8d3d55c9f458542b8d9f852b
robertla234 <robertla234@yahoo.com>
2021-03-21 19:12:37 -0700

    add v2

---
+++ Java Spring Boot/tracker_v2/src/main/java/com/example/tracker_v2/TrackerV2Application.java
@@ -0,0 +1,13 @@
+package com.example.tracker_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerV2Application.class, args);
+	}
+
+}
---
+++ Java Spring Boot/tracker_v2/src/test/java/com/example/tracker_v2/TrackerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tracker_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
