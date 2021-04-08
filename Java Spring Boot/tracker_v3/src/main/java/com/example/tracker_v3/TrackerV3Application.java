53d13006623956d4c062847b5fd430bb1e222400
robertla234 <robertla234@yahoo.com>
2021-04-08 14:48:58 -0700

    Start of Tracker v3

---
+++ Java Spring Boot/tracker_v3/src/main/java/com/example/tracker_v3/TrackerV3Application.java
@@ -0,0 +1,13 @@
+package com.example.tracker_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerV3Application.class, args);
+	}
+
+}
---
+++ Java Spring Boot/tracker_v3/src/test/java/com/example/tracker_v3/TrackerV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tracker_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
